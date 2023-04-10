701257ec5a8baa9d8925da92d114c099947a5190
Simiola <70043967+Simiola@users.noreply.github.com>
2023-04-10 10:06:57 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,57 +1,31 @@
 import socket
-from  threading import Thread
-import time
+from threading import Thread
 
-
-
-
+SERVER = None
 IP_ADDRESS = '127.0.0.1'
 PORT = 8050
-SERVER = None
 BUFFER_SIZE = 4096
 clients = {}
 
-
-
 def acceptConnections():
-    global SERVER
-    global clients
+  global SERVER, clients
 
-    while True:
-        client, addr = SERVER.accept()
-        client_name = client.recv(4096).decode().lower()
-        clients[client_name] = {
-                "client"         : client,
-                "address"        : addr,
-                "connected_with" : "",
-                "file_name"      : "",
-                "file_size"      : 4096
-            }
+  while True:
+    client, addr = SERVER.accept()
+    print(client,addr)
 
-        print(f"Connection established with {client_name} : {addr}")
-
-        
 def setup():
-    print("\n\t\t\t\t\t\tIP MESSENGER\n")
+  print("\n\t\t\t\t\t\tIP MESSENGER\n")
 
-    global PORT
-    global IP_ADDRESS
-    global SERVER
+  global SERVER, IP_ADDRESS, PORT
 
+  SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  SERVER.bind((IP_ADDRESS, PORT))
+  SERVER.listen(100)
 
-    SERVER  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    SERVER.bind((IP_ADDRESS, PORT))
+  print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...\n")
 
-    SERVER.listen(100)
-
-    print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...")
-    print("\n")
-
-    acceptConnections()
-
-
-
+  acceptConnections()
 
 setup_thread = Thread(target=setup)
 setup_thread.start()
-
