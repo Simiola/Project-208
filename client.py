86d649e112dec81127a2d4d87627bed1518c6bf8
Simiola <70043967+Simiola@users.noreply.github.com>
2023-04-10 10:06:26 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,82 +1,54 @@
 import socket
 from threading import Thread
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, filedialog
 
-
-PORT  = 8050
-IP_ADDRESS = '127.0.0.1'
 SERVER = None
+IP_ADDRESS = '127.0.0.1'
+PORT = 8050
 BUFFER_SIZE = 4096
 
-name = None
-listbox =  None
-filePathLabel = None
+font = "Calibri"
 
-global song_counter
-song_counter = 0
+def musicWindow():
+  window = Tk()
+  window.title("Music Window")
+  window.geometry("300x300")
+  window.configure(bg="#87CEFA")
 
-  
+  selectLabel = Label(window, text="Select Song", bg="#87CEFA", font=(font, 8))
+  selectLabel.place(x=2, y=1)
 
-   
-    
-    
-    
-def musicWindow(): 
-    global song_counter
-    global filePathLabel
-    global listbox
-    global infoLabel
-    
-    window=Tk()
-    window.title('Music Window')
-    window.geometry("300x300")
-    window.configure(bg='LightSkyBlue')
-    
-    selectlabel = Label(window, text= "Select Song",bg='LightSkyBlue', font = ("Calibri",8))
-    selectlabel.place(x=2, y=1)
-    
-    listbox = Listbox(window,height = 10,width = 39,activestyle = 'dotbox',bg='LightSkyBlue',borderwidth=2, font = ("Calibri",10))
-    listbox.place(x=10,y=18)
-   
-        
-    scrollbar1 = Scrollbar(listbox)
-    scrollbar1.place(relheight = 1,relx = 1)
-    scrollbar1.config(command = listbox.yview)
-    
-    PlayButton=Button(window,text="Play", width=10,bd=1,bg='SkyBlue',font = ("Calibri",10), )
-    PlayButton.place(x=30,y=200)
-    
-    Stop=Button(window,text="Stop",bd=1,width=10,bg='SkyBlue', font = ("Calibri",10), )
-    Stop.place(x=200,y=200)
-    
-    Upload=Button(window,text="Upload",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10))
-    Upload.place(x=30,y=250)
-    
-    Download =Button(window,text="Download",width=10,bd=1,bg='SkyBlue', font = ("Calibri",10))
-    Download.place(x=200,y=250)
-    
-    infoLabel = Label(window, text= "",fg= "blue",bg='SkyBlue', font = ("Calibri",8))
-    infoLabel.place(x=4, y=280)
-    
-    window.mainloop()
-    
+  listbox = Listbox(window, height=10, width=39, activestyle="dotbox", bg="#87CEFA", borderwidth=2, font=(font, 10))
+  listbox.place(x=10, y=18)
+
+  scrollbar1 = Scrollbar(listbox)
+  scrollbar1.place(relheight=1, relx=1)
+  scrollbar1.config(command=listbox.yview)
+
+  playButton = Button(window, text="Play", width=10, bg="#87CEEB", bd=1, font=(font, 10))
+  playButton.place(x=30, y=200)
+
+  stopButton = Button(window, text="Stop", width=10, bg="#87CEEB", bd=1, font=(font, 10))
+  stopButton.place(x=200, y=200)
+
+  uploadButton = Button(window, text="Upload", width=10, bg="#87CEEB", bd=1, font=(font, 10))
+  uploadButton.place(x=30, y=250)
+
+  downloadButton = Button(window, text="Download", width=10, bg="#87CEEB", bd=1, font=(font, 10))
+  downloadButton.place(x=200, y=250)
+
+  infoLabel = Label(window, text="", fg="#00F", font=(font, 8))
+  infoLabel.place(x=4, y=200)
+
+  window.mainloop()
+
+
 def setup():
-    global SERVER
-    global PORT
-    global IP_ADDRESS
-    global song_counter
+  global SERVER, IP_ADDRESS, PORT
+  SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  SERVER.connect((IP_ADDRESS, PORT))
 
-    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    SERVER.connect((IP_ADDRESS, PORT))
+  musicWindow()
 
-    musicWindow()
-
-  
 setup()
-
-
-   
-
-
-
